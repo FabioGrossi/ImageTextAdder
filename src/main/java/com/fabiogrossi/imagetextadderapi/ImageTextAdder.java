bf36dde3e9e2c085a131dc4603d6b65428dcf6a6
Fabio <diecinstoria@gmail.com>
2021-10-31 01:18:52 +0200

    Remove redundant code

--- src/main/java/com/fabiogrossi/imagetextadderapi/ImageTextAdder.java
+++ src/main/java/com/fabiogrossi/imagetextadderapi/ImageTextAdder.java
@@ -32,55 +32,17 @@ public class ImageTextAdder {
     }
 
     public static BufferedImage drawTextInCenteredSquareWithOffset(BufferedImage image, String text, Font font, int rectangleWidth, int rectangleHeight, int offsetX, int offsetY) {
-        Graphics2D graphics2D = (Graphics2D) image.getGraphics();
-
         int imageWidth = image.getWidth();
         int imageHeight = image.getHeight();
 
         int rectanglePositionX = (imageWidth / 2) - (rectangleWidth / 2) + offsetX;
         int rectanglePositionY = (imageHeight / 2) - (rectangleHeight / 2) + offsetY;
 
-        Rectangle rectangle = new Rectangle(rectanglePositionX, rectanglePositionY, rectangleWidth, rectangleHeight);
-        BrokenText brokenText = TextBreakingUtils.breakText(text, font, graphics2D.getFontRenderContext(), rectangleWidth);
-
-        float y = (float) (rectangle.getY() + (rectangleHeight - brokenText.getTextHeight()) / 2);
-
-        for (TextLayout line : brokenText.getLines()) {
-            Rectangle2D bounds = line.getBounds();
-            float x = (float) (rectangle.getX() + (rectangleWidth - bounds.getWidth()) / 2);
-
-            line.draw(graphics2D, x, y + line.getAscent());
-
-            y += line.getAscent() + line.getDescent() + line.getLeading();
-        }
-        graphics2D.dispose();
-        return image;
+        return drawTextInSquare(image, text, font, rectanglePositionX, rectanglePositionY, rectangleWidth, rectangleHeight);
     }
 
     public static BufferedImage drawTextInCenteredSquare(BufferedImage image, String text, Font font, int rectangleWidth, int rectangleHeight) {
-        Graphics2D graphics2D = (Graphics2D) image.getGraphics();
-
-        int imageWidth = image.getWidth();
-        int imageHeight = image.getHeight();
-
-        int rectanglePositionX = (imageWidth / 2) - (rectangleWidth / 2);
-        int rectanglePositionY = (imageHeight / 2) - (rectangleHeight / 2);
-
-        Rectangle rectangle = new Rectangle(rectanglePositionX, rectanglePositionY, rectangleWidth, rectangleHeight);
-        BrokenText brokenText = TextBreakingUtils.breakText(text, font, graphics2D.getFontRenderContext(), rectangleWidth);
-
-        float y = (float) (rectangle.getY() + (rectangleHeight - brokenText.getTextHeight()) / 2);
-
-        for (TextLayout line : brokenText.getLines()) {
-            Rectangle2D bounds = line.getBounds();
-            float x = (float) (rectangle.getX() + (rectangleWidth - bounds.getWidth()) / 2);
-
-            line.draw(graphics2D, x, y + line.getAscent());
-
-            y += line.getAscent() + line.getDescent() + line.getLeading();
-        }
-        graphics2D.dispose();
-        return image;
+        return drawTextInCenteredSquareWithOffset(image, text, font, rectangleWidth, rectangleHeight, 0, 0);
     }
 
 }
